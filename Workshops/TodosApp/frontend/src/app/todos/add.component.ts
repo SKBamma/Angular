bc2620865c80e6701183aa2b8029c758903d6006
Suresh Bamma <sureshbamma321@gmail.com>
2024-06-10 16:37:54 -0500

    initial set up

---
+++ Workshops/TodosApp/frontend/src/app/todos/add.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      add works!
+    </p>
+  `,
+  styles: ``
+})
+export class AddComponent {
+
+}
